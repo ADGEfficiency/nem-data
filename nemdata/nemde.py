a449372e202dbf96f4fd9c515004786669a83aac
Adam Green <adam.green@adgefficiency.com>
2022-12-09 22:04:22 +1300

    rm nemde neu

--- nemdata/nemde.py
+++ nemdata/nemde.py
@@ -1,60 +1,109 @@
-import os
-from pathlib import Path
+import datetime
+import pathlib
+import typing
+import warnings
 
+import numpy as np
 import pandas as pd
+import pydantic
+import requests
+from rich import print
 
-from nemdata.utils import URL, add_interval_cols, download_zipfile_from_url, unzip
+import nemdata
+from nemdata import utils
+from nemdata.config import DEFAULT_BASE_DIRECTORY
 
 
-def make_nemde_url(year, month, day, base_dir):
-    month = str(month).zfill(2)
-    day = str(day).zfill(2)
+class NEMDEFile(pydantic.BaseModel):
+    year: int
+    month: int
+    day: int
+    url: str
+    xml_name: str
+    data_directory: pathlib.Path
+    zipfile_path: pathlib.Path
 
-    home = base_dir / "nemde" / f"{year}-{month}-{day}"
-    home.mkdir(exist_ok=True, parents=True)
 
-    return URL(
-        url=f"http://www.nemweb.com.au/Data_Archive/Wholesale_Electricity/NEMDE/{year}/NEMDE_{year}_{month}/NEMDE_Market_Data/NEMDE_Files/NemPriceSetter_{year}{month}{day}_xml.zip",
+def make_many_nemde_files(
+    start: str, end: str, base_directory: pathlib.Path
+) -> list[NEMDEFile]:
+    """creates many NEMDEFiles - one for each day"""
+
+    files = []
+    months = pd.date_range(start=start, end=end, freq="D")
+    for year, month, day in zip(months.year, months.month, months.day):
+        files.append(
+            make_one_nemde_file(
+                year=year, month=month, day=day, base_directory=base_directory
+            )
+        )
+    return files
+
+
+def make_one_nemde_file(
+    year: int, month: int, day: int, base_directory: pathlib.Path
+) -> NEMDEFile:
+
+    padded_month = str(month).zfill(2)
+    padded_day = str(day).zfill(2)
+
+    url = f"http://www.nemweb.com.au/Data_Archive/Wholesale_Electricity/NEMDE/{year}/NEMDE_{year}_{padded_month}/NEMDE_Market_Data/NEMDE_Files/NemPriceSetter_{year}{padded_month}{padded_day}_xml.zip"
+
+    xml_name = f"NemPriceSetter_{year}{padded_month}{padded_day}.xml"
+
+    data_directory = base_directory / "nemde" / f"{year}-{padded_month}-{padded_day}"
+    data_directory.mkdir(exist_ok=True, parents=True)
+
+    return NEMDEFile(
         year=year,
         month=month,
-        report="nemde",
-        csv=None,
-        xml=f"NemPriceSetter_{year}{month}{day}.xml",
-        home=home,
+        day=day,
+        url=url,
+        xml_name=xml_name,
+        data_directory=data_directory,
+        zipfile_path=data_directory / "raw.zip",
     )
 
 
-def make_many_nemde_urls(start, end, base_dir):
-    urls = []
-    months = pd.date_range(start=start, end=end, freq="D")
-    for year, month, day in zip(months.year, months.month, months.day):
-        urls.append(make_nemde_url(year, month, day, base_dir))
-    return urls
-
-
 def find_xmls(path):
-    fis = [p for p in Path(path).iterdir() if p.suffix == ".xml"]
+    fis = [p for p in path.iterdir() if p.suffix == ".xml"]
     return [pd.read_xml(f) for f in fis]
 
 
-def download_nemde(start, end, report_id, base_dir):
-    assert report_id == "nemde"
-    urls = make_many_nemde_urls(start, end, base_dir)
-    output = []
-    for url in urls:
-        zf = download_zipfile_from_url(url)
-        unzip(zf)
-        xmls = find_xmls(url.home)
-        clean = pd.concat(xmls, axis=0)
+def download_nemde(
+    start: str,
+    end: str,
+    base_directory: pathlib.Path = DEFAULT_BASE_DIRECTORY,
+):
+    """main for downloading MMSDMFiles"""
+    files = make_many_nemde_files(start, end, base_directory)
+    dataset = []
+    for file in files:
+        clean_fi = file.data_directory / "clean.parquet"
+        if clean_fi.exists():
+            print(f" [blue]EXISTS[/] {' '.join(clean_fi.parts[-5:])}")
+            data = pd.read_parquet(clean_fi)
+        else:
+            print(f" [blue]MISSING[/] {' '.join(clean_fi.parts[-5:])}")
+            print(f" [green]DOWNLOADING[/] {' '.join(file.zipfile_path.parts[-5:])}")
+            utils.download_zipfile(file)
+            utils.unzip(file.zipfile_path)
+            xmls = find_xmls(file.data_directory)
+            data = pd.concat(xmls, axis=0)
+            #  get problems with a value of '5' without the cast to float
+            data["BandNo"] = data["BandNo"].astype(float)
 
-        #  get problems with a value of '5' without the cast to float
-        clean["BandNo"] = clean["BandNo"].astype(float)
+            #  accounting for AEMO stamping intervals at the end
+            #  usually intervals are stamped at the start
+            data["PeriodID"] = pd.to_datetime(data["PeriodID"]).dt.tz_localize(None)
+            data = utils.add_interval_cols(data, "PeriodID", "5T")
 
-        #  accounting for AEMO stamping intervals at the end
-        #  usually intervals are stamped at the start
-        clean["PeriodID"] = pd.to_datetime(clean["PeriodID"]).dt.tz_localize(None)
-        clean = add_interval_cols(clean, "PeriodID", "5T")
+            print(f" [green]SAVING [/] {clean_fi}")
+            data.to_csv(clean_fi.with_suffix(".csv"))
+            data.to_parquet(clean_fi.with_suffix(".parquet"))
+        dataset.append(data)
+    return pd.concat(dataset, axis=0)
 
-        print(f" saving csv and parquet to {url.home}/clean")
-        clean.to_csv(Path(url.home) / "clean.csv")
-        clean.to_parquet(Path(url.home) / "clean.parquet")
+
+if __name__ == "__main__":
+    download_nemde(start="2021-10-01", end="2021-10-01")
