4816d358bec201b51d2304d2132bc81b47a402ec
Adam Green <adam.green@adgefficiency.com>
2023-01-02 08:13:02 +1300

    CLI cleanup, README cleanup

--- nemdata/cli.py
+++ nemdata/cli.py
@@ -9,17 +9,29 @@ from nemdata.downloader import download
 @click.option(
     "--table",
     "-t",
-    help="nemde, " + ", ".join([table.name for table in mmsdm.mmsdm_tables]),
+    help="Available data is nemde, "
+    + ", ".join([table.name for table in mmsdm.mmsdm_tables])
+    + ".",
 )
-@click.option("--start", "-s", default="2018-01", help="start date (YYYY-MM)")
-@click.option("--end", "-e", default="2018-03", help="end date (incusive) (YYYY-MM)")
+@click.option(
+    "--start",
+    "-s",
+    default="2018-01",
+    help="Start date (YYYY-MM or YYYY-MM-DD for NEMDE).",
+)
+@click.option(
+    "--end",
+    "-e",
+    default="2018-03",
+    help="End date (incusive) (YYYY-MM or YYYY-MM-DD for NEMDE).",
+)
 @click.option(
     "--dry-run/--no-dry-run",
     default=False,
-    help="whether to save downloaded data to disk",
+    help="Whether to save downloaded data to disk.",
 )
 def cli(start: str, end: str, table: str, dry_run: bool) -> None:
-    """nemdata is a tool to access NEM data from AEMO."""
+    """Downloads NEM data from AEMO."""
     print(":wave: from nemdata\n")
     download(start, end, table, dry_run=dry_run)
 
